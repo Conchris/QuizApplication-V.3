a18a1f5001e78020afba992455c5879b40e20e7f
conchris <dodi.kabeya.30@gmail.com>
2019-04-20 06:59:59 +0300

    changed

---
+++ Sport/sport.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+  $("#question1").click(function(){
+    $("#answer1").slideToggle("slow");
+    $("#answer2").slideToggle("slow");
+    $("#answer3").slideToggle("slow");
+  });
+});
+$(document).ready(function(){
+  $("#question2").click(function(){
+    $("#answer4").slideToggle("slow");
+    $("#answer5").slideToggle("slow");
+    $("#answer6").slideToggle("slow");
+  });
+});
+$(document).ready(function(){
+  $("#question3").click(function(){
+    $("#answer7").slideToggle("slow");
+    $("#answer8").slideToggle("slow");
+    $("#answer9").slideToggle("slow");
+  });
+});
+$(document).ready(function(){
+  $("#question4").click(function(){
+    $("#answer10").slideToggle("slow");
+    $("#answer11").slideToggle("slow");
+    $("#answer12").slideToggle("slow");
+  });
+});
+$(document).ready(function(){
+  $("#question5").click(function(){
+    $("#answer13").slideToggle("slow");
+    $("#answer14").slideToggle("slow");
+    $("#answer15").slideToggle("slow");
+  });
+});
+
+function dodi() {
+  var a=document.forms["myform"]["question_1"].value;
+  var b=document.forms["myform"]["question_2"].value;
+  var c=document.forms["myform"]["question_3"].value;
+if(a==""||b==""||c==""){
+  alert("Please Question  can't be left Empty");
+  return false;
+}
+var a2=document.forms["myform"]["question_4"].value;
+var b2=document.forms["myform"]["question_5"].value;
+var c2=document.forms["myform"]["question_6"].value;
+if(a2==""||b2==""||c2==""){
+alert("Please Question  can't be left Empty");
+return false;
+}
+var a3=document.forms["myform"]["question_7"].value;
+var b3=document.forms["myform"]["question_8"].value;
+var c3=document.forms["myform"]["question_9"].value;
+if(a3==""||b3==""||c3==""){
+alert("Please Question  can't be left Empty");
+return false;
+}
+
+
+}
---
+++ main.php
@@ -0,0 +1,19 @@
+<?xml version = "1.0" encoding = "utf-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd>
+<html xmlns = "http://www.w3.org/1999/xhtml">
+  <head>
+    <link rel="stylesheet" href="Main.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="tabledesign">
+
+          <p></i><i class="fas fa-paint-brush"></i><a href="Art/art.php">ART</a></p>
+          <p><i class="fas fa-globe"></i><a href="History/history.php">HISTORY</a></p>
+          <p><i class="fas fa-atom"></i><a href="science/science.php">SCIENCE</a></p>
+          <p><i class="fas fa-futbol"></i><a href="Sport/sport.php">SPORT</a></p>
+    </div>
+  </body>
+</html>
